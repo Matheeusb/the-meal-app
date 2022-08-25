a3fad12a6c7731302f3a2838b447b0397d14406b
Matheus Bereta <matheus_bereta@hotmail.com>
2022-08-25 10:19:33 -0300

    Desafio Técnico

--- lib/features/details/repositories/details.repository.dart
+++ lib/features/details/repositories/details.repository.dart
@@ -5,14 +5,12 @@ import '../../../exceptions/network_exception.dart';
 import '../../../models/meal_details.dart';
 
 class MealDetailsRepository {
-  final Dio _dio;
-
-  MealDetailsRepository(this._dio);
+  MealDetailsRepository();
 
   Future<List<MealDetails>> getMealDetails(String idMeal) async {
     try {
       final response =
-          await _dio.get('$theMealUrlBase${'lookup.php?i=$idMeal'}');
+          await Dio().get('$theMealUrlBase${'lookup.php?i=$idMeal'}');
       return response.data['meals']
           .map<MealDetails>((mealDetails) => MealDetails.fromJson(mealDetails))
           .toList();
